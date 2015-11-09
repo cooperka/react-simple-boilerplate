f423b3fa7173dbe29efbb149aa4728e445ab799e
Kevin Cooper <cooperka92@gmail.com>
2015-11-08 21:11:17 -0800

    Introduce basic style loading support

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,6 +1,8 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+require('!style!css!./style/hello-world.css');
+
 ReactDOM.render(
   React.createElement('h1', null, 'Hello, world!'),
   document.getElementById('example')
