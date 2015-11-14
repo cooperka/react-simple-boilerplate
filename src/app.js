c6407a28d794542686e398ac186be32dee0bd764
Kevin Cooper <cooperka92@gmail.com>
2015-11-13 21:20:10 -0800

    Add normalize.css

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/hello-world';
 
+require('./style/app.scss');
+
 class App extends React.Component {
 
   render() {
