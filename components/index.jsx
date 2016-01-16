8a1d0eb00f9b66b43a4ac94051111d37e915d1d8
Kevin Cooper <cooperka92@gmail.com>
2016-01-16 11:27:11 -0800

    Upgrade redux-devtools to v3.0

--- components/index.jsx
+++ components/index.jsx
@@ -3,28 +3,22 @@ import 'normalize.css/normalize.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { devTools, persistState } from 'redux-devtools';
-import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
-import reducer from '../redux/reducers';
 import App from './App';
+import DevTools from './DevTools';
+import configureStore from '../redux/configureStore';
 
-const store = compose(
-  devTools(),
-  // Lets you write ?debug_session=<name> in address bar to persist debug sessions.
-  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
-)(createStore)(reducer);
+const store = configureStore();
 
 const rootComponent = (
   <div>
     <Provider store={store}>
-      <App />
+      <div>
+        <App />
+        <DevTools />
+      </div>
     </Provider>
-    <DebugPanel top right bottom>
-      <DevTools store={store} monitor={LogMonitor} />
-    </DebugPanel>
   </div>
 );
 
---
+++ redux/configureStore.js
@@ -0,0 +1,36 @@
+import { createStore, compose } from 'redux';
+import { persistState } from 'redux-devtools';
+
+import reducer from './reducers';
+import DevTools from '../components/DevTools';
+
+const finalCreateStore = compose(
+  // Middleware can also be applied here.
+
+  // Required! Enable Redux DevTools with the monitors you set up in DevTools.
+  DevTools.instrument(),
+
+  persistState(getDebugSessionKey())
+)(createStore);
+
+/**
+ * Lets you write ?debug_session=<name> in address bar to persist debug sessions.
+ * @returns {string|null} The debug session key.
+ */
+function getDebugSessionKey() {
+  const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);
+  return (matches && matches.length > 0) ? matches[1] : null;
+}
+
+export default function configureStore(initialState) {
+  const store = finalCreateStore(reducer, initialState);
+
+  // Hot reload reducers
+  if (module.hot) {
+    module.hot.accept('./reducers', () =>
+      store.replaceReducer(require('./reducers') /* .default if you use Babel 6+ */)
+    );
+  }
+
+  return store;
+}
