f374eaaba7e22ecd18dd2c50c1d2a1261bc2a1dc
Kevin Cooper <cooperka92@gmail.com>
2017-01-25 21:47:51 -0500

    :rocket: Add babel-stage-3 preset for spread operators

--- redux/reducers.js
+++ redux/reducers.js
@@ -2,8 +2,6 @@ import Immutable from 'immutable';
 
 import { ADD_THING } from './actions';
 
-const { assign } = Object;
-
 const initialState = {
   things: Immutable.List(),
 };
@@ -11,9 +9,10 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case ADD_THING:
-      return assign({}, state, {
+      return {
+        ...state,
         things: state.things.push(action.name),
-      });
+      };
 
     default:
       return state;
